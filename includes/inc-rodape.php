1c889c467a37ecea9ccde2bae91f1572a0b796a3
Danilo Santos <danilofla@gmail.com>
2015-08-17 20:33:46 -0300

    Exclusão de arquivos e diretórios desncessários

--- includes/inc-rodape.php
+++
@@ -1,4 +0,0 @@
-<div id="copyright" class="left">
-	<p>Danilo Santos - Todos os Direitos Reservados<br /><a href="mailto: job@danilosantos.cc" class="email">job@danilosantos.cc</a><br />+55 81.8772-8857</p>	
-	<a href="http://br.linkedin.com/in/dsantosweb" title="Acesse meu Perfil no LinkedIn" target="_blank"><img src="<?= URL; ?>/imagens/icone-linkedin-colorido.png" height="30" width="30" alt="" /></a>
-</div>
